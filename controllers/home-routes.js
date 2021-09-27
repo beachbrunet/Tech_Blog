f9c0c5659d6a82e3a45b524f271ac2252219b8d4
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-27 10:33:45 -0600

    saving home route code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const { User, Post, Comment } = require("../models");
 const sequelize = require("../config/connection");
 
 // Starting here --- techBlog_db
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
   console.log(req.session);
 
   Post.findAll({
@@ -52,30 +52,45 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-// example code
-// const galleries = dbGalleryData.map((gallery) =>
-// gallery.get({ plain: true })
-// );
+router.get("/post route", (req, res) => {
+  console.log(req.session);
 
-// res.render('homepage', {
-// galleries,
-// loggedIn: req.session.loggedIn,
-// });
-// } catch (err) {
-// console.log(err);
-// res.status(500).json(err);
-// }
-// });
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: [["id", "post_id", "created_at", "post_content"]],
+        inlcude: {
+          model: User,
+          attributes: ["username"],
+        },
+        // would like to add a socialmedia attribute as a future feature.
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.statis(404).json("Not Found");
+        return;
+      }
 
-// This is signup example code
-// router.get("signup", async (request, response) => {
-//   try {
-//     response.render("signup", { logged_in: request.session.logged_in });
-//   } catch (err) {
-//     console.log(err);
-//     response.status(500).json(err);
-//   }
-// });
+      // serialize data to be stored
+      const posts = dbPostData.get({ plain: true });
+      //
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.export = router;
 
