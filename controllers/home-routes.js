b509809c2d84162bb99226f6a1e5a4c235157074
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-26 21:04:18 -0600

    Working on user and home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,48 +1,63 @@
-const router = require("express").Router();
 // User, Post, Comment
+const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
 const bcrypt = require("bcrypt");
-const { request, response } = require('express');
 
 //Base code
-router.get("/", async (req, res) => {
-  try {
-    const  = await Post.findAll({
-      include: [
-        {
-          model: Painting,
-          attributes: ["filename", "description"],
-        },
-      ],
-    });
+// router.get('/gallery/:id', async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect('/login');
+//   } else {
+//     // If the user is logged in, allow them to view the gallery
+//     try {
+//       const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: Painting,
+//             attributes: [
+//               'id',
+//               'title',
+//               'artist',
+//               'exhibition_date',
+//               'filename',
+//               'description',
+//             ],
+//           },
+//         ],
+//       });
+//       const gallery = dbGalleryData.get({ plain: true });
+//       res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
 
-    const galleries = dbGalleryData.map((gallery) =>
-      gallery.get({ plain: true })
-    );
-
-    res.render("homepage", {
-      galleries,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+// Starting here
+router.get("/", (req, res) => {
+  console.log(req.session);
+  Post.FindAll({
+    // per told via TA
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: [["id", "title", "created_at", "post_content"]],
+      },
+    ],
+  });
 });
 
-
-// comments
-
-// login, should redirect if already logged in
-
-// sign up
-router.get('signup', async (request, response) => {
-  try {
-      response.render('signup', { logged_in: request.session.logged_in })
-  } catch (err) {
-      console.log(err);
-      response.status(500).json(err);
-  }
-})
+// This is signup example code
+// router.get("signup", async (request, response) => {
+//   try {
+//     response.render("signup", { logged_in: request.session.logged_in });
+//   } catch (err) {
+//     console.log(err);
+//     response.status(500).json(err);
+//   }
+// });
 
 module.export = router;
