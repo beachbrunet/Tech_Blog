5c53cbcea985229dfc49c707882536c8b932e722
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-27 10:41:02 -0600

    renamed for index.js in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,16 @@
 // boilerplate code
 const router = require("express").Router();
-
-const userRoutes = require("./user-routes");
+const userRoutes = require("./api");
+const userRoutes = require("./dashboard-route");
+const userRoutes = require("./dashboard-route");
 
 router.use("/users", userRoutes);
+router.use("/users", userRoutes);
+router.use("/users", userRoutes);
+
+// added error
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
