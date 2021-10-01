15c75b4a1053be2c887dc75d0d38a43dce313336
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 13:40:47 -0600

    fixed minor errors

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -1,43 +1,44 @@
 // get, post, delete
 const router = require("express").Router();
-const {Comment } = require("../models");
+const { Comment } = require("../models");
 const withAuth = require("../utils/authentication");
 
 // get
-router.get('/', (req, res) => {
-    comment.findAll({})
-    .then(db => res.json(db))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+router.get("/", (req, res) => {
+  Comment.findAll({})
+    .then((db) => res.json(db))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // post
-router.post('/', withAuth (req, res) => {
-    if (req.session) {
-        comment.create({
-            comment_text: req.body.comment_text,
-            post_id: req.body.post_id,
-            // need user b/c we are using the id from the session
-            user_id: req.session.user_id,
-        })
-        .then(db => res.json(db))
-        .catch(err => {
+router.post("/", withAuth, (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // need user b/c we are using the id from the session
+      user_id: req.session.user_id,
+    })
+      .then((db) => res.json(db))
+      .catch((err) => {
         console.log(err);
         res.status(500).json(err);
       });
-    }
+  }
 });
 
 // example code
-router.post("/", async (req, res) => {
-    try {
-      const userData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-      });
+// router.post("/", async (req, res) => {
+//     try {
+//       const userData = await User.create({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password,
+//       });
 
-
+//     }
 // delete
