f75c420898ab5068003ff6b79b4174c6bf93a723
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 13:29:54 -0600

    working on comments.js get, post, delete routes

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -1,4 +1,19 @@
 // get, post, delete
-const
-const
-const
+const router = require("express").Router();
+const {Comment } = require("../models");
+const withAuth = require("../utils/authentication");
+
+// get
+router.get('/', (req, res) => {
+    comment.findAll({})
+    .then(db => res.json(db))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+}
+
+// post
+
+
+// delete
