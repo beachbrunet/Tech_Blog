c09d9a3853a33dd837a0abc29b8f95f2a4baab30
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 13:32:56 -0600

    working on comments.js Get syntax

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -11,9 +11,24 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+// post
+router.post('/', async (req, res) => {
+    comment.findAll({})
+    .then(db => res.json(db))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 }
 
-// post
+router.post("/", async (req, res) => {
+    try {
+      const userData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
 
 
 // delete
