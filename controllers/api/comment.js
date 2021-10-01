40ac3ec82ec2ddd8bab422ca0a7746e4e4878110
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-01 13:39:43 -0600

    working on comments.js POSt syntax

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -12,16 +12,25 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
 });
+
 // post
-router.post('/', async (req, res) => {
-    comment.findAll({})
-    .then(db => res.json(db))
-    .catch(err => {
+router.post('/', withAuth (req, res) => {
+    if (req.session) {
+        comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // need user b/c we are using the id from the session
+            user_id: req.session.user_id,
+        })
+        .then(db => res.json(db))
+        .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
-}
+    }
+});
 
+// example code
 router.post("/", async (req, res) => {
     try {
       const userData = await User.create({
